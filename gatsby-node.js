7feb8ddcc1464474e6bea8949c2694a7f5227d13
Joe Hart <joehart42@outlook.com>
2020-02-23 15:47:11 +0000

    Add Custom types for Tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,31 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  const { createTypes } = actions
+  const typeDefs = [
+    `type BooksJson implements Node {
+        tags: [TagsJson]! @link(by: "slug")
+    }`,
+
+    `type TagsJson implements Node {
+        isSpoiler: Boolean!
+    }`,
+  ]
+  createTypes(typeDefs)
+}
+
+exports.createResolvers = ({ createResolvers }) => {
+  const resolvers = {
+    TagsJson: {
+      isSpoiler: {
+        type: "Boolean!",
+        resolve(source, args, context, info) {
+          return !!source.isSpoiler
+        },
+      },
+    },
+  }
+  createResolvers(resolvers)
+}
